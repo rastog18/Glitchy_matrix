af030cd9de05674d3aa6d91ef7a2b78c006c527d
Shivam Rastogi <130913444+rastog18@users.noreply.github.com>
2023-05-02 00:08:16 +0530

    Update PD15_Coffeemachine.py

--- PD15_Coffeemachine.py
+++ PD15_Coffeemachine.py
@@ -1,29 +1,3 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-
 resources = {
     "water_count": 300,
     "milk_count": 200,
